d314ce90a59074216602ef5c3a88d232f850f509
Sololiquy <77843814+Sololiquy@users.noreply.github.com>
2024-11-04 18:13:36 +0700

    Update statDescription.tsx

--- pages/components/student detail/statDescription.tsx
+++ pages/components/student detail/statDescription.tsx
@@ -46,7 +46,7 @@ const StatDescription = ({}) => {
             <div className={styles.role}>
                 <div className={styles.starGrade}>
                     {[...Array(data?.StarGrade)].map((_, i) => (
-                        <img className={styles.starIMG} key={i} src="/star.svg" alt="" />
+                        <img className={styles.starIMG} key={i} src="/blue-archive-wiki/star.svg" alt="" />
                     ))}
                 </div>
                 <div className={`${styles.typeSquad} ${squadType[0]}`}>{squadType[1]}</div>
